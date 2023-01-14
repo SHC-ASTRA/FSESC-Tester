1e490bd1161349bfc97af8283baba10772125793
Spacedays <44454593+Spacedays@users.noreply.github.com>
2023-01-14 13:16:52 -0600

    Added manual RPM command

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int motorSpd = 0;
 float maxPower = 0.5;
 float motorPower = 0;
-int MAX_SPEED = 6000;
+int MAX_SPEED = 6000; // [RPM]
 int interval = 1000;
 bool enableDriver;
 bool vescStatus;
@@ -26,7 +26,7 @@ void printVESCStatus();
 //*******************************
 // Command Handling
 //*******************************
-#define cmdSerial Serial1	// Serial port to receive commands from - USB Port (virtual)
+#define cmdSerial Serial	// Serial port to receive commands from - USB Port (virtual)
 // #define cmdSerial Serial1	// Serial port to receive commands from - pico pins GP0&GP1
 
 void parseCommand(String command);
@@ -77,7 +77,9 @@ void loop()
 
 	if (cmdSerial.available())
 	{
+		cmdSerial.print("Received cmd: ");
 		String command = cmdSerial.readStringUntil('\n');
+		cmdSerial.println(command);
 		parseCommand(command);
 		
 	}
@@ -87,7 +89,7 @@ void loop()
 		lastRcvTime = millis();
 		motorSpd;
 		lastSendTime = millis();
-		Serial.println("Status: Speed:"+motorSpd);
+		cmdSerial.println("Status: Speed:"+motorSpd);
 	}
 
 	if (millis() - lastRcvTime > 200)
@@ -98,32 +100,50 @@ void loop()
 	VESC1.setRPM(motorSpd);
 }
 
-// Command Format: [exec_cmd];
+// Command Format: [commmand_type],power_str
+// Commands: max_power, esc_status
 void parseCommand(String command)
 {
-	String exec = command.substring(0, command.indexOf(';'));
+	// String exec = command.substring(0, command.indexOf(';'));
 		lastRcvTime = millis();
-	if (exec.equals("set_motor") && enableDriver)
+	// cmdSerial.println(command);	//#DEBUG
+	// cmdSerial.println(exec);		//#DEBUG
+
+	lastRcvTime = millis();
+
+	int firstComma = command.indexOf(',');
+	String motor = command.substring(0, firstComma);
+	String power_str = command.substring(firstComma + 1);
+	float power = strtof(power_str.c_str(), NULL);	// Power, Max power, RPM value
+
+	if (motor.equals("RPM"))
 	{
-		lastRcvTime = millis();
-
-		int firstComma = command.indexOf(',');
-		String motor = command.substring(exec.length() + 1, firstComma);
-		String power_str = command.substring(firstComma + 1);
-		float power = strtof(power_str.c_str(), NULL);
-
-		if (motor.equals("max_power"))
-		{
-			maxPower = power;
-		}
-		else if (motor.equals("front_right"))
-		{
-			motorPower = power;
-		}
-		motorSpd = (int)constrain(motorPower * MAX_SPEED * maxPower, -MAX_SPEED, MAX_SPEED);
+		motorSpd = (int)constrain(power, -MAX_SPEED, MAX_SPEED);
+		cmdSerial.print("Set RPM to ");
+		cmdSerial.println(motorSpd);
+	} else 
+	{
+	if (motor.equals("set_power"))
+	{
+		motorPower = power;
+		cmdSerial.print("Set power to ");
+		cmdSerial.println(motorPower);
 	}
-	if (exec.equals("script_status"))
+	else if (motor.equals("max_power"))
 	{
+		maxPower = power;
+		cmdSerial.print("Set max power to ");
+		cmdSerial.println(power);
+	}
+
+	motorSpd = (int)constrain(motorPower * MAX_SPEED * maxPower, -MAX_SPEED, MAX_SPEED);
+	cmdSerial.print("New Motor speed: ");
+	cmdSerial.println(motorSpd);
+	}
+
+	if (motor.equals("esc_status"))
+	{
+		cmdSerial.println("Detailed status:");
 		printVESCStatus();
 	}
 }
